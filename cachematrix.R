dac4beecb133956a806320574217985f684ca98f
Rajat Gupta <rgupta592@gmail.com>
2020-04-25 02:24:29 +0530

    Add files via upload
    SUbmitted programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,11 @@
+##The first function, makeVector creates a special "vector", which is really a list containing a function to
+#	1.	set the value of the matrix
+#	2.	get the value of the matrix
+# 3.   set the value of the matrix
+#4.    get the value of the inverse
+
+
+
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -16,6 +24,8 @@ makeCacheMatrix <- function(x = matrix()) {
 #print(x)
 }
 
+#The following function calculates the mean of the special "matrix" created with the above function. However, it first checks to see if the inverse has already been calculated. If so, it gets the inverse from the cache and skips the computation. Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via the setmean function.
+
 cacheSolve <- function(x, ...) {
         m <- x$getinverse()
         #print(m)
@@ -29,3 +39,7 @@ cacheSolve <- function(x, ...) {
         m
       
 }
+
+
+#source("Inversematrix assignment_rajat.R")
+#> cacheSolve(makeCacheMatrix(matrix(c(4,2,7,6),2,2)))
